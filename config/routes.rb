178ab777dc6e95ba5660c38a2dd57151f568257b
katee <hello@kate.io>
2015-03-17 22:12:39 -0400

    Moves content over from other site. Not yet styled.

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
 
   resources :purchases
 
+  # Static pages
+  get 'about' => 'static_pages#about'
+  get 'membership' => 'static_pages#membership'
+  get 'classes' => 'static_pages#classes'
+  get 'projects' => 'static_pages#projects'
+  get 'location' => 'static_pages#location'
   root to: "static_pages#home"
 end
