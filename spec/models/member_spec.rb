f1d5b8faa17574e35ac63311bfe20eaee81db8b2
katee <hello@kate.io>
2015-03-19 19:26:53 -0400

    Uses  insteaed of should.

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -2,12 +2,12 @@ RSpec.describe Member, type: :model do
   context "validations" do
     context "must have a name" do
       subject { build(:member, name: nil) }
-      its(:valid?) { should eq(false) }
+      its(:valid?) { is_expected.to eq(false) }
     end
 
     context "must have an email" do
       subject { build(:member, email: nil) }
-      its(:valid?) { should eq(false) }
+      its(:valid?) { is_expected.to eq(false) }
     end
   end
 end
