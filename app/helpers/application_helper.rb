82fc4b9cef692fc1621bb32f3c10e342eae52184
katee <hello@kate.io>
2015-03-18 20:34:17 -0400

    Adds titles to static pages.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def title(page_title)
+    content_for :title, [page_title.to_s, t('site_name')].select(&:present?).join(" | ")
+  end
+
   def body_classes
     [
       controller_name,
