d5aa80f98a5b07596004275d015a0dd8855de651
katee <hello@kate.io>
2015-03-19 23:46:12 -0400

    Adds versions migration and schema changes.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150314183538) do
+ActiveRecord::Schema.define(version: 20150320034421) do
 
   create_table "members", force: :cascade do |t|
     t.text     "name",                                   null: false
@@ -35,5 +35,16 @@ ActiveRecord::Schema.define(version: 20150314183538) do
 
   add_index "members", ["email"], name: "index_members_on_email", unique: true
   add_index "members", ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+
+  create_table "versions", force: :cascade do |t|
+    t.string   "item_type",  null: false
+    t.integer  "item_id",    null: false
+    t.string   "event",      null: false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+  end
+
+  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
 
 end
