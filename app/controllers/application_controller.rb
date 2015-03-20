ee7db3bc9647f1507fd6e1875f36fba90991266e
katee <hello@kate.io>
2015-03-19 23:47:08 -0400

    Set versions to use current_member.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def user_for_paper_trail
+    current_member
+  end
 end
