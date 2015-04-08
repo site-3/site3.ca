24aa9f7bc57c3434e5eb9012e7d871b17f39a9a1
katee <hello@kate.io>
2015-04-07 20:03:39 -0400

    Don't require the password to update a user.

--- app/controllers/members/registrations_controller.rb
+++ app/controllers/members/registrations_controller.rb
@@ -3,6 +3,11 @@ class Members::RegistrationsController < Devise::RegistrationsController
   before_filter :configure_account_update_params, only: [:update]
 
 protected
+  # Don't reqire the password to update a Member
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
+
   def after_update_path_for(member)
     edit_member_registration_path
   end
