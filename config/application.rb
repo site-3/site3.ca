200276b5392faa3a0841b0257ecf73ba7d0647c2
katee <hello@kate.io>
2015-11-15 16:31:09 -0500

    Sets default url for mailer correctly.

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Site3Members
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.action_mailer.default_url_options = { host: ENV['MAILER_HOST'] }
   end
 end
