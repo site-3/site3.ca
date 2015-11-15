e001b8394ff93ece186e74232f575e43d0d5b290
katee <hello@kate.io>
2015-11-14 22:32:01 -0500

    Adds API to create MemberApplications. Creates Stripe customers for the MemberApplication.

--- app/controllers/member_applications_controller.rb
+++ app/controllers/member_applications_controller.rb
@@ -1,4 +1,6 @@
 class MemberApplicationsController < ApplicationController
+  protect_from_forgery unless: -> { request.format.json? }
+
   def new
     @member_application = MemberApplication.new
   end
@@ -7,17 +9,40 @@ class MemberApplicationsController < ApplicationController
     @member_application = MemberApplication.new(member_application_params)
 
     if @member_application.valid?
-      @member_application.save!
-      # TODO show success
-      redirect_to after_create_path
-    else
-      render :new
+      @stripe_customer = Stripe::Customer.create(
+        email: @member_application.email,
+        description: @member_application.name,
+        source: @member_application.stripe_payment_token,
+      )
+
+      @member_application.stripe_id = @stripe_customer.id
+    end
+
+    respond_to do |format|
+      format.json do
+        if @member_application.save
+          render json: {success: true}
+        else
+          render json: {
+            success: false,
+            errors: @member_application.errors,
+          }, status: 522
+        end
+      end
+      format.html do
+        if @member_application.save
+          # TODO show success
+          redirect_to after_create_path
+        else
+          render :new
+        end
+      end
     end
   end
 
 private
   def member_application_params
-    params.require(:member_application).permit(:name, :email)
+    params.require(:member_application).permit(:name, :email, :rfid, :stripe_payment_token, :vending_machine_enabled)
   end
 
   def after_create_path
--- spec/controllers/member_applications_controller_spec.rb
+++ spec/controllers/member_applications_controller_spec.rb
@@ -1,4 +1,10 @@
+require 'stripe_mock'
+
 RSpec.describe MemberApplicationsController, type: :controller do
+  let(:stripe_helper) { StripeMock.create_test_helper }
+  before { StripeMock.start }
+  after { StripeMock.stop }
+
   describe "#new" do
     def go!
       post :new
@@ -11,24 +17,104 @@ RSpec.describe MemberApplicationsController, type: :controller do
   end
 
   describe "#create" do
+    def go!(params)
+      post :create, params
+    end
+
     context "with all required params" do
+      let(:stripe_payment_token) { stripe_helper.generate_card_token }
       let(:member_application) { FactoryGirl.build(:member_application) }
       let(:member_application_params) do
         {
           member_application: {
             name: member_application.name,
             email: member_application.email,
+            stripe_payment_token: stripe_payment_token,
+            enable_vending_machine: true,
           }
         }
       end
 
-      def go!(params)
-        post :create, params
+      it "creates a MemberApplication" do
+        expect{ go!(member_application_params) }.to change(MemberApplication, :count).by(1)
+      end
+
+      it "creates a Stripe::Customer" do
+        expect(Stripe::Customer).to receive(:create).with({
+          email: member_application.email,
+          description: member_application.name,
+          source: stripe_payment_token,
+        }).and_call_original
+
+        go!(member_application_params)
+
+        expect(MemberApplication.last.stripe_id).to be_present
       end
 
       it "redirects to root_path" do
         go!(member_application_params)
+
         expect(response).to redirect_to(root_path)
+      end
+
+      context "with json format" do
+        before do
+          @request.headers['Accept'] = 'application/json'
+        end
+
+        it "returns appropriate json" do
+          go!(member_application_params)
+
+          expect(response).to be_ok
+          response_json = JSON.parse(response.body).deep_symbolize_keys
+          expect(response_json).to eq({success: true})
+        end
+      end
+    end
+
+    context "with missing params" do
+      let(:member_application_params) do
+        {
+          member_application: {
+            name: "Test Person"
+          }
+        }
+      end
+
+      it "does not create a MemberApplication" do
+        expect{ go!(member_application_params) }.not_to change(MemberApplication, :count)
+      end
+
+      it "does not create a Stripe::Customer" do
+        expect(Stripe::Customer).not_to receive(:create)
+
+        go!(member_application_params)
+      end
+
+      it "renders new" do
+        go!(member_application_params)
+
+        expect(response).to be_ok
+        expect(response).to render_template(:new)
+      end
+
+      context "with json format" do
+        before do
+          @request.headers['Accept'] = 'application/json'
+        end
+
+        it "returns appropriate json" do
+          go!(member_application_params)
+
+          expect(response.status).to eq(522)
+          response_json = JSON.parse(response.body).deep_symbolize_keys
+          expect(response_json).to eq({
+            success: false,
+            errors: {
+              email: ["can't be blank"]
+            }
+          })
+        end
       end
     end
   end
